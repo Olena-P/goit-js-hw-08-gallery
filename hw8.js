9655d3e64b55100ae73c272d91d15c7b35f470c3
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-04-11 21:10:53 +0300

    .

--- hw8.js
+++ hw8.js
@@ -1,6 +1,19 @@
 // - Создание и рендер разметки по массиву данных и предоставленному шаблону.
 import images from './gallery-items.js';
 
+const refs = {
+    gallery: document.querySelector(".js-gallery"),
+    modal: document.querySelector(".lightbox__content"),
+    lightbox: document.querySelector(".lightbox"),
+    button: document.querySelector('[data-action="close-lightbox"]'),
+    lightbox__image: document.querySelector(".lightbox__image"),
+    lightbox__overlay: document.querySelector(".lightbox__overlay"),
+};
+
+
+// - Реализация делегирования на галерее `ul.js-gallery` 
+// и получение`url` большого изображения.
+//(вставить разметку в  ul)
 const createGalleryItem = ({ preview, original, description}) => {
     return `<li class="gallery__item">
         <a
@@ -16,38 +29,23 @@ const createGalleryItem = ({ preview, original, description}) => {
         </a>
     </li>`
 };    
- 
-const refs = {
-    gallery: document.querySelector(".js-gallery"),
-    modal: document.querySelector(".lightbox__content"),
-    lightbox: document.querySelector(".lightbox"),
-    button: document.querySelector('[data-action="close-lightbox"]'),
-    lightbox__image: document.querySelector(".lightbox__image"),
-    lightbox__overlay: document.querySelector(".lightbox__overlay"),
-};
   
 //перебрать и присоединить
 const imagesTemplateStrings = images.map(createGalleryItem).join(" ");
-console.log(imagesTemplateStrings)
 
 // вставить в html
 refs.gallery.insertAdjacentHTML('afterbegin', imagesTemplateStrings);
 
-
 //добавление элементов галереи
 refs.gallery.addEventListener('click', onOpenModal);
-refs.modal.addEventListener('click', closeModalClick); 
+refs.lightbox__overlay.addEventListener('click', closeModalClick); 
 refs.button.addEventListener('click', onCloseModal);
-lightbox__overlay
-
-// - Реализация делегирования на галерее `ul.js-gallery` 
-// и получение`url` большого изображения.
-//(вставить разметку в  ul)
 
 // - Открытие модального окна по клику на элементе галереи.
 // - Подмена значения атрибута `src` элемента `img.lightbox__image`.
 function onOpenModal(e) {
     e.preventDefault();
+
     if (e.target.nodeName !== 'IMG') {
         return;
     };
@@ -56,29 +54,27 @@ function onOpenModal(e) {
         refs.lightbox.classList.add('is-open');
         refs.lightbox__image.src = e.target.getAttribute('data-source');
         refs.lightbox__image.alt = e.target.getAttribute('alt');
-    }
+    };
+
+    window.addEventListener('kaydown', onCloseModalEsc);
+    // window.addEventListener('keydown', onArrowPress);
 
     //неподвижный фон
     document.body.style.overflow = ('hidden'); 
-    // setOriginalImageOnLightbox(e);
-    // addOpenLightboxClass();
 
-    // window.addEventListener('kaydown', onCloseModal);
-    // window.addEventListener('keydown', onArrowPress);
 }
 
 // - Закрытие модального окна по клику на кнопку `button[data-action="close-lightbox"]`.
 
 function onCloseModal(e) {
-    
-        refs.lightbox.classList.remove('is-open');
-        refs.lightbox__image.src = e.target.getAttribute(' ');
-        refs.lightbox__image.alt = e.target.getAttribute(' ');
+    refs.lightbox.classList.remove('is-open');
+    refs.lightbox__image.src = e.target.getAttribute(' ');
+    refs.lightbox__image.alt = e.target.getAttribute(' ');
 
-        document.body.removeAttribute('Style');
-        // window.removeEventListener('kaydown', onCloseModalEsc);
-        // window.removeEventListener('keydown', onArrowPress);
-   
+    document.body.removeAttribute('Style');
+    window.removeEventListener('kaydown', onCloseModalEsc);
+    //(переключатель) window.removeEventListener('keydown', onArrowPress);
+
 }
 
 // - Очистка значения атрибута `src` элемента `img.lightbox__image`. 
@@ -95,8 +91,8 @@ function onCloseModal(e) {
 // - Закрытие модального окна по клику на `div.lightbox__overlay`.
 
 function closeModalClick (e) {
-    if (e.target === refs.lightboxOverlay) {
-        onCloseModal()
+    if (e.target === e.currentTarget) {
+        onCloseModal();
     }
 }
 
@@ -104,7 +100,7 @@ function closeModalClick (e) {
 // - Закрытие модального окна по нажатию клавиши `ESC`.
 function onCloseModalEsc(e) {
     if (e.code === 'Escape') {
-        onCloseModal()
+        onCloseModal();
     }
 }
 
@@ -114,4 +110,7 @@ function onCloseModalEsc(e) {
 
 
 // Управление кнопками //
+
+
+
 
