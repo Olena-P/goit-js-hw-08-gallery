8c1d54e2b44d39f8c29e8c60540cc9478db4a435
Olena Pavliuk <73667823+Olena-P@users.noreply.github.com>
2021-04-11 14:35:57 +0300

    /

--- hw8.js
+++ hw8.js
@@ -36,22 +36,23 @@ refs.gallery.insertAdjacentHTML('afterbegin', imagesTemplateStrings);
 
 
 //добавление элементов галереи
+
 // ulRef.innerHTML = newStringEl;
-// const imgEl = document.querySelector('.gallery__image');
+const imgEl = document.querySelector('.gallery__image');
 // ulRef.addEventListener('click', e); 
 
 
 // - Реализация делегирования на галерее `ul.js-gallery` и получение `url` большого
 //   изображения.
 
-// images.addEventListener('click', onClick);
-// function onClick(e) {
-//     if (e.target.nodeName !== 'image') {
-//         return
-//     }
-//     console.log(e.target);
-// }
-
+images.addEventListener('click', onClick);
+function onClick(e) {
+    if (e.target.nodeName !== 'image') {
+        return
+    }
+    console.log(e.target);
+}
+///////////////////// 
 
 
 // - Открытие модального окна по клику на элементе галереи.
@@ -193,87 +194,3 @@ document.addEventListener('keydown', eve => {
   }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// "use strict";
-// import images from "./gallery-items.js";
-
-// const refs = {
-//   gallery: document.querySelector(".js-gallery"),
-//   image: document.createElement("img"),
-//   lightbox: document.querySelector(".lightbox"),
-//   btn: document.querySelector('[data-action="close-lightbox"]'),
-//   modal: document.querySelector(".lightbox__content"),
-//   lightbox__image: document.querySelector(".lightbox__image"),
-// };
-
-// const createGalleryItem = ({ preview, original, description }) =>
-//   `<li class="gallery__item">
-// <a
-//   class="gallery__link"
-//   href=${original}
-// >
-//   <img
-//     class="gallery__image"
-//     src=${preview}
-//     data-source=${original}
-//     alt=${description}
-//   />
-// </a>
-// </li>`;
-// const galleryMarkup = images.reduce(
-//   (acc, item) => acc + createGalleryItem(item),
-//   ""
-// );
-// refs.gallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-// refs.image.classList.add("gallery__image");
-
-// refs.gallery.addEventListener("click", onGalleryClick);
-// refs.btn.addEventListener("click", onClickHandlerClose);
-// refs.modal.addEventListener("click", closeLightbox);
-
-// function onGalleryClick(e) {
-//   e.preventDefault();
-//   if (e.target.nodeName !== 'IMG') {
-//     return;
-//   }
-//   if (e.target.nodeName === "IMG") {
-//     refs.lightbox.classList.add("is-open");
-//     refs.lightbox__image.src = e.target.getAttribute("data-source");
-//     refs.lightbox__image.alt = e.target.alt;
-//   }
-//   window.addEventListener("keyup", clickKey);
-// }
-
-// function onClickHandlerClose(e) {
-//   e.preventDefault(); 
-//   refs.lightbox.classList.remove("is-open");
-//   refs.lightbox__image.src = '';
-//   refs.lightbox__image.alt = '';
-//   window.removeEventListener("keyup", clickKey);
-// }
-
-// function closeLightbox(event) {
-//   if (event.target === event.currentTarget) {
-//     onClickHandlerClose();
-//   }
-// }
-
-// function clickKey(event) {
-//   if (event.code === "Escape") {
-//     onClickHandlerClose();
-//   }
-// }
